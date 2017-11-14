38b01db349db2a1a65038750351a038793f106db
mrjuoss <mrjuoss@gmail.com>
2017-11-14 08:36:09 +0700

    Belajar method map dalam sebuah array

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 let https = require('https')
 
 let profile = require('./profile.js')
-// atau import profile from '.profile.js'
+// atau import profile from './profile.js'
 // Sebelum kita bisa import sebuat module, maka module tersebut harus diexport terlebih dahulu
 
-profile.get('rizafahmi')
-profile.get('mrjuoss')
-profile.get('user-ngawur')
+/*
+  profile.get('rizafahmi')
+  profile.get('mrjuoss')
+  profile.get('user-ngawur')
+*/
+
+/*
+  Jika ingin seperti ini
+  let profiles = ['rizafahmi', 'mrjuoss', 'user-ngawur']
+*/
+
+/* Bermain dengan Array Map */
+/*
+  Array Map berguna untuk mengubah arrat baru. Misal kita punya Array [1,2,3] dan ingin mengubahnya menjadi Array baru dengan nilai [2,4,6] maka solusinya adalah masing-masing array harus dikalikan dengan 2.
+
+  Contoh
+*/
+
+let array_lama = [1,2,4]
+
+let array_baru = array_lama.map(item => {
+  return item * 2
+})
+
+console.log(array_baru)
