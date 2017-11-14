d7273d1d6f0281c89528a0deee4b3069ce5196c7
Jaki <mrjuoss@gmail.com>
2017-11-14 08:27:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let https = require('https')
 
 let profile = require('./profile.js')
-// atau import profile from '.profile.js'
+// atau import profile from './profile.js'
 // Sebelum kita bisa import sebuat module, maka module tersebut harus diexport terlebih dahulu
 
 profile.get('rizafahmi')
