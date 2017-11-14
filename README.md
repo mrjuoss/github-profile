6b058f381522e43629b73ae2a04a50ca906d7da7
mrjuoss <mrjuoss@gmail.com>
2017-11-14 08:24:18 +0700

    Finish refactor to module profile

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Solution : Use NodeJS to connect GITHUB API to get profile info and print out to
 *[x] Read the data
 *[x] Parse the data
 *[x] Print the data out
+
+## REFACTOR
+
+Refactor adalah membuat Code yang sudah jalan agar lebih RAPI, lebih MODULAR
+
+Refactor pertama - Ngeprint klo Usernya Eror/Tidak Ditemukan
+
+Refactor kedua - Lebih MODULAR, Lebih enak manggilnya dan bisa dipanggil berkali - kali. Seperti profile.get('username')
--- app.js
+++ app.js
@@ -1,69 +1,9 @@
-// TODO : [x] Connect to Github API
 let https = require('https')
 
-const username = 'rizafahmi'
+let profile = require('./profile.js')
+// atau import profile from '.profile.js'
+// Sebelum kita bisa import sebuat module, maka module tersebut harus diexport terlebih dahulu
 
-const options = {
-  hostname: 'api.github.com',
-  port: 443,
-  path: `/users/${username}`,
-  method: 'GET',
-  headers: {
-    'user-agent': 'nodejs'
-  }
-}
-
-// [x] TODO : Read the data
-let request = https.request(options, (response) => {
-  let body = ''
-  //console.log('Got response : ', response)
-  response.on('data', (data) => {
-    body = body + data
-  })
-  response.on('end', () => {
-    //console.log(body)
-    //console.log(typeof(body))
-    let profile = JSON.parse(body)
-
-    if (response.statusCode === 200) {
-      //[x] TODO :Parse the data
-        // Convert String to JSON (Javascript Object)
-
-        //console.log(profile.login + ' Owns ' + profile.public_repos + ' repo(s)')
-        //console.log(typeof(profile))
-        //[x] TODO : Print the data out
-        console.log(`${profile.login} owns ${profile.public_repos} repo(s) and has  ${profile.followers} follower(s).`)
-    } else {
-      console.log(`Profile with username '${username}' not found.`)
-    }
-
-  })
-})
-
-request.end()
-
-request.on('error', (e) => {
-  console.log(e)
-})
-
-// Beberapa Cara membuat function di Javascript
-// Cara 1 = Cara lama
-function namaFungsi(param) {
-  console.log('fungsi ini dipanggil ' + param)
-}
-
-// Cara 2 = Anonymous Function yang di assign ke sebuat variabel deng keyword let
-let namaFungsi2 = function() {
-  console.log('Ini adalah fungsi tanpa nama');
-}
-
-// Cara 3 = Disebut Arrow Function. Merupakan shortcut dari cara ke 2
-let namaFungsi3 = () => {
-  console.log('Arrow function')
-}
-
-namaFungsi('hallo')
-
-namaFungsi2()
-
-namaFungsi3()
+profile.get('rizafahmi')
+profile.get('mrjuoss')
+profile.get('user-ngawur')
